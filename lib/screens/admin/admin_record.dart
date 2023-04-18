8b387a71ddea3845c033bd7fcb44ba6c84e3b933
Lans12345 <olanalans12345@gmail.com>
2023-04-18 12:46:38 +0800

    fetched data in records

--- lib/screens/admin/admin_record.dart
+++ lib/screens/admin/admin_record.dart
@@ -1,4 +1,5 @@
 import 'package:car_rental/widgets/text_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AdminRecord extends StatelessWidget {
@@ -26,68 +27,107 @@ class AdminRecord extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            SingleChildScrollView(
-              child: DataTable(columns: [
-                DataColumn(
-                  label: TextBold(
-                      text: 'Personel', fontSize: 18, color: Colors.black),
-                ),
-                DataColumn(
-                  label: TextBold(
-                      text: 'Type of Vehicle',
-                      fontSize: 18,
-                      color: Colors.black),
-                ),
-                DataColumn(
-                  label: TextBold(
-                      text: 'Plate Number', fontSize: 18, color: Colors.black),
-                ),
-                DataColumn(
-                  label: TextBold(
-                      text: 'Total Distance\nTravelled',
-                      fontSize: 18,
-                      color: Colors.black),
-                ),
-                DataColumn(
-                  label: TextBold(
-                      text: 'Total Fuel\nUsed',
-                      fontSize: 18,
-                      color: Colors.black),
-                ),
-                DataColumn(
-                  label: TextBold(
-                      text: 'Remarks', fontSize: 18, color: Colors.black),
-                ),
-              ], rows: [
-                DataRow(cells: [
-                  DataCell(
-                    TextRegular(
-                        text: 'John Doe', fontSize: 14, color: Colors.grey),
-                  ),
-                  DataCell(
-                    TextRegular(
-                        text: 'Tesla Model X',
-                        fontSize: 14,
-                        color: Colors.grey),
-                  ),
-                  DataCell(
-                    TextRegular(
-                        text: '123456', fontSize: 14, color: Colors.grey),
-                  ),
-                  DataCell(
-                    TextRegular(text: '23km', fontSize: 14, color: Colors.grey),
-                  ),
-                  DataCell(
-                    TextRegular(
-                        text: '5 liters', fontSize: 14, color: Colors.grey),
-                  ),
-                  DataCell(
-                    TextRegular(
-                        text: 'Done', fontSize: 14, color: Colors.green),
-                  ),
-                ])
-              ]),
-            ),
+            StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('Request')
+                    .where('status', isEqualTo: 'Approved')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    print('error');
+                    return const Center(child: Text('Error'));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Center(
+                          child: CircularProgressIndicator(
+                        color: Colors.black,
+                      )),
+                    );
+                  }
+
+                  final data = snapshot.requireData;
+                  return SingleChildScrollView(
+                    child: DataTable(columns: [
+                      DataColumn(
+                        label: TextBold(
+                            text: 'Personel',
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                      DataColumn(
+                        label: TextBold(
+                            text: 'Type of Vehicle',
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                      DataColumn(
+                        label: TextBold(
+                            text: 'Plate Number',
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                      DataColumn(
+                        label: TextBold(
+                            text: 'Total Distance\nTravelled',
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                      DataColumn(
+                        label: TextBold(
+                            text: 'Total Fuel\nUsed',
+                            fontSize: 18,
+                            color: Colors.black),
+                      ),
+                      DataColumn(
+                        label: TextBold(
+                            text: 'Remarks', fontSize: 18, color: Colors.black),
+                      ),
+                    ], rows: [
+                      for (int i = 0; i < data.docs.length; i++)
+                        DataRow(cells: [
+                          DataCell(
+                            TextRegular(
+                                text: data.docs[i]['name'],
+                                fontSize: 14,
+                                color: Colors.grey),
+                          ),
+                          DataCell(
+                            TextRegular(
+                                text: data.docs[i]['vehicle'],
+                                fontSize: 14,
+                                color: Colors.grey),
+                          ),
+                          DataCell(
+                            TextRegular(
+                                text: data.docs[i]['vehicleTemplate'],
+                                fontSize: 14,
+                                color: Colors.grey),
+                          ),
+                          DataCell(
+                            TextRegular(
+                                text: 'Not available',
+                                fontSize: 14,
+                                color: Colors.grey),
+                          ),
+                          DataCell(
+                            TextRegular(
+                                text: 'Not available',
+                                fontSize: 14,
+                                color: Colors.grey),
+                          ),
+                          DataCell(
+                            TextRegular(
+                                text: 'Approved',
+                                fontSize: 14,
+                                color: Colors.green),
+                          ),
+                        ])
+                    ]),
+                  );
+                }),
           ],
         ),
       ),
