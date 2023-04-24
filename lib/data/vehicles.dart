eb4be75d632e8dc0e8174e238252c5537efd981d
Lans12345 <olanalans12345@gmail.com>
2023-04-24 20:25:04 +0800

    added vehicles data

--- lib/data/vehicles.dart
+++ lib/data/vehicles.dart
@@ -1,143 +1,87 @@
 import '../model/vehicle_model.dart';
 
-final List<Car> cars = [
+List<Car> cars = [
   Car(
     make: 'Toyota',
-    model: 'Corolla',
-    year: 2022,
+    model: 'Fortuner',
+    year: 2021,
     imageUrl:
-        'https://www.toyota.com/imgix/responsive/images/mlp/colorizer/2022/corolla/1H3/car.png?bg=fff&fm=webp&w=930',
-  ),
-  Car(
-    make: 'Honda',
-    model: 'Civic',
-    year: 2022,
-    imageUrl:
-        'https://www.automobiles.honda.com/-/media/Honda-Automobiles/Vehicles/2022/Civic-Sedan/Feature-New/2022-HON-Civic-Sedan-Feature-1-944x590.png',
-  ),
-  Car(
-    make: 'Ford',
-    model: 'Mustang',
-    year: 2022,
-    imageUrl:
-        'https://www.ford.com/cmslibs/content/dam/vdm_ford/live/en_us/ford/nameplate/mustang/2022/collections/highlights/3_2/22_FRD_Mustang_MLP_Hero_Shot_1600x900_V1.jpg/_jcr_content/renditions/cq5dam.web.1280.1280.jpeg',
-  ),
-  Car(
-    make: 'Chevrolet',
-    model: 'Corvette',
-    year: 2022,
-    imageUrl:
-        'https://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2022/performance/corvette-stingray/colorizer/01-images/2022-chevrolet-corvette-stingray-colorizer-01.png',
-  ),
-  Car(
-    make: 'BMW',
-    model: '3 Series',
-    year: 2022,
-    imageUrl:
-        'https://www.bmwusa.com/content/dam/bmwusa/XModels/3Series/2022/3Series.jpg',
-  ),
-  Car(
-    make: 'Mercedes-Benz',
-    model: 'C-Class',
-    year: 2022,
-    imageUrl:
-        'https://www.mercedes-benz.com/en/vehicles/passenger-cars/c-class/saloon/design-keyvisuals/vision-1-c-klasse-design-key-visuals-9-16x9-retina.jpg',
-  ),
-  Car(
-    make: 'Audi',
-    model: 'A4',
-    year: 2022,
-    imageUrl:
-        'https://www.audiusa.com/content/dam/audiusa/en/homepage/hero-desktop/A4-MY22-HERO-2880x1620.jpg',
-  ),
-  Car(
-    make: 'Lexus',
-    model: 'ES',
-    year: 2022,
-    imageUrl:
-        'https://www.lexus.com/cm-img/visualizer/2022/es/atomic-silver/m1z-1.jpg',
-  ),
-  Car(
-    make: 'Mazda',
-    model: 'Mazda3',
-    year: 2022,
-    imageUrl:
-        'https://www.mazdausa.com/siteassets/vehicles/2022/m3h/01_images/22-m3h-dyn-red-ext-hero.jpg',
-  ),
-  Car(
-    make: 'Subaru',
-    model: 'Impreza',
-    year: 2022,
-    imageUrl:
-        'https://www.subaru.com/content/dam/subaru/vehicles/impreza/2022/overview/impreza22-overview-blue-02.jpg',
+        'https://www.toyota.com.ph/wp-content/uploads/2020/10/fortuner_2021_1.jpg',
   ),
   Car(
     make: 'Kia',
-    model: 'Stinger',
-    year: 2022,
+    model: 'Sportage',
+    year: 2021,
     imageUrl:
-        'https://www.kia.com/content/dam/kia/mx/es/images/2022/stinger/m360/overview/mobile/kia-stinger-m360-overview-01-mobile.jpg',
+        'https://www.kia.com/content/dam/kwcms/kme/global/en/assets/gnb/main/Sportage%202.0L%20UX%20Plus.png',
+  ),
+  Car(
+    make: 'Toyota',
+    model: 'Avanza',
+    year: 2021,
+    imageUrl: 'https://toyota.com.ph/sites/default/files/2021-07/Avanza.png',
+  ),
+  Car(
+    make: 'Toyota',
+    model: 'Innova',
+    year: 2021,
+    imageUrl: 'https://toyota.com.ph/sites/default/files/2021-07/Innova.png',
+  ),
+  Car(
+    make: 'Toyota',
+    model: 'T-Hi-Ace Grandia',
+    year: 2021,
+    imageUrl: 'https://toyota.com.ph/sites/default/files/2021-07/Grandia.png',
+  ),
+  Car(
+    make: 'Toyota',
+    model: 'T-Hi-Ace Commuter',
+    year: 2021,
+    imageUrl: 'https://toyota.com.ph/sites/default/files/2021-07/Commuter.png',
+  ),
+  Car(
+    make: 'Mitsubishi',
+    model: 'L300',
+    year: 2021,
+    imageUrl:
+        'https://www.mitsubishi-motors.com.ph/images/all-vehicles/l300-cab-and-chassis-mobile.png',
+  ),
+  Car(
+    make: 'Toyota',
+    model: 'Hilux',
+    year: 2021,
+    imageUrl: 'https://toyota.com.ph/sites/default/files/2021-07/Hilux.png',
+  ),
+  Car(
+    make: 'Toyota',
+    model: 'T.T.FX. Revo SR',
+    year: 2021,
+    imageUrl: 'https://toyota.com.ph/sites/default/files/2021-07/Revo.png',
+  ),
+  Car(
+    make: 'Hino',
+    model: 'Truck Van',
+    year: 2021,
+    imageUrl: 'https://hino.ph/wp-content/uploads/2019/05/3381_1.jpg',
+  ),
+  Car(
+    make: 'Hino',
+    model: 'Bus New',
+    year: 2021,
+    imageUrl: 'https://hino.ph/wp-content/uploads/2020/08/3384.jpg',
   ),
   Car(
     make: 'Hyundai',
-    model: 'Elantra',
-    year: 2022,
+    model: 'County',
+    year: 2021,
     imageUrl:
-        'https://www.hyundaiusa.com/content/dam/hyundai/us/en/vehicles/2022/elantra-sedan/overview/22-ela-4dr-sel-phantom-black.png',
+        'https://www.hyundai.ph/wp-content/uploads/2021/02/Hyundai-County.jpg',
   ),
   Car(
-    make: 'Nissan',
-    model: 'Maxima',
-    year: 2022,
+    make: 'Willys',
+    model: 'Jeep',
+    year: 2021,
     imageUrl:
-        'https://www.nissanusa.com/content/dam/Nissan/us/vehicles/maxima/2022/overview/2022-nissan-maxima-color-studio-12.png.ximg.l_12_m.smart.png',
-  ),
-  Car(
-    make: 'Porsche',
-    model: '911',
-    year: 2022,
-    imageUrl:
-        'https://www.porsche.com/usa/_shared/img/model-pages/911/911-turbo-s/colors/modelcolorsgallery/paa/2022/turbo-s/06_2022-911-TURBO-S-GRANDE-PAA.webp',
-  ),
-  Car(
-    make: 'Tesla',
-    model: 'Model S',
-    year: 2022,
-    imageUrl: 'https://www.tesla.com/xNVh4yUEc3B9/02_Desktop.jpg',
-  ),
-  Car(
-    make: 'Cadillac',
-    model: 'CT5',
-    year: 2022,
-    imageUrl:
-        'https://www.cadillac.com/content/dam/cadillac/na/us/english/index/vehicles/2022/ct5/sport/more/ct5-22-sport-more-bgrd-2880x1800.jpg',
-  ),
-  Car(
-    make: 'Infiniti',
-    model: 'Q50',
-    year: 2022,
-    imageUrl:
-        'https://www.infiniti.com/content/dam/Infiniti/global/vehicles/q50/q50h/tuner/21TDI-Q50H-Tuner-1920x1080-01.jpg.ximg.l_full_m.smart.jpg',
-  ),
-  Car(
-    make: 'Jaguar',
-    model: 'XF',
-    year: 2022,
-    imageUrl:
-        'https://www.jaguarusa.com/content/dam/jaguar/master/en/index/vehicles/xf/2022/XF-Hero-1-Desktop.jpg',
-  ),
-  Car(
-    make: 'Volvo',
-    model: 'S90',
-    year: 2022,
-    imageUrl:
-        'https://www.volvocars.com/images/v/us/v/-/media/project/contentplatform/data/media/img/2021/06/s90-21-gallery-01-16x9.jpg?iar=0&w=1366&iar=0',
-  ),
-  Car(
-    make: 'Maserati',
-    model: 'Ghibli',
-    year: 2022,
-    imageUrl:
-        'https://www.maseratiusa.com/content/dam/maserati/international/my21/en/hero/Ghibli_MY21_hero.jpg',
-  ),
+        'https://www.willysjeep.com.ph/wp-content/uploads/2019/09/140123.jpg',
+  )
 ];
